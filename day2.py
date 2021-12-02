899f5b7394793c97a446f6cd70037010fe5f7043
Lance Fannin <lancefannin@gmail.com>
2021-12-02 13:29:33 -0500

    day 2

---
+++ day2.py
@@ -0,0 +1,20 @@
+input = open("resources/day2-input.txt").read().split("\n")
+
+horizontal = 0
+depth = 0
+aim = 0
+
+for step in input:
+    s = step.split()
+    distance = int(s[1])
+    if s[0] == 'forward':
+        horizontal = horizontal + distance
+        depth = depth + (aim * distance)
+    elif s[0] == 'up':
+        aim = aim - distance
+    elif s[0] == 'down':
+        aim = aim + distance
+    
+    print('{0},{1},{2} - {3}'.format(horizontal, depth, aim, step))
+
+print(horizontal*depth)
