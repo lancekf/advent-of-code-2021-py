a38e9bbdf8c1d42a2f37bf9a7f891590b77a6f0e
Lance Fannin <lancefannin@gmail.com>
2021-12-02 10:52:10 -0500

    Add code to generated python script for reading test input data

--- new-day.py
+++ new-day.py
@@ -4,6 +4,8 @@ day = int(sys.argv[1])
 
 assert day > 0, f"Day must be greater than 0, was: {day}"
 
-open(f"day{day}.py", "w")
+f = open(f"day{day}.py", "w")
+f.write(f'input = open("resources/day{day}-input-test.txt").read().split("\\n")\n\n')
+
 open(f"resources/day{day}-input.py", "w")
 open(f"resources/day{day}-input-test.py", "w")
