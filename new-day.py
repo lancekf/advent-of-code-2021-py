b10bdafd7c8b4de8d1ad3eb70451d3a8fc811948
Lance Fannin <lancefannin@gmail.com>
2021-12-03 10:58:44 -0500

    move files and update new-day.py to use dayN/partX.py file structure

--- new-day.py
+++ new-day.py
@@ -1,11 +1,17 @@
 import sys
+import os
 
 day = int(sys.argv[1])
 
 assert day > 0, f"Day must be greater than 0, was: {day}"
 
-f = open(f"day{day}.py", "w")
-f.write(f'input = open("resources/day{day}-input-test.txt").read().split("\\n")\n\n')
+day = f"day{day}"
 
-open(f"resources/day{day}-input.txt", "w")
-open(f"resources/day{day}-input-test.txt", "w")
+os.makedirs(day, exist_ok=True)
+
+f = open(f"{day}/part1.py", "w")
+f.write('input = open("input-test.txt").read().split("\\n")\n\n')
+
+open(f"{day}/part2.py", "w")
+open(f"{day}/input.txt", "w")
+open(f"{day}/input-test.txt", "w")
